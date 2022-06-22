fb703c789b4f693dba6e96fd08e4a143bcd7c2d7
Angel <a20190192@pucp.edu.pe>
2022-06-22 01:08:24 -0500

    index, avance regiones

--- detalleRegion/detalleRegion.js
+++ detalleRegion/detalleRegion.js
@@ -3,5 +3,37 @@ $(document).ready(function () {
     // Metodo de obtención de parámetros
     const urlParams = new URLSearchParams(window.location.search);
     const idRegion = urlParams.get('region');
+    var locations = [];
+    var actual = 1;
+    var paginas;
+    var pagul = $('#paginacion');
+
+    $.ajax({
+        method: "GET",
+        url:"https://pokeapi.co/api/v2/region/"+idRegion
+    }).done((response)=>{
+        locations = response.locations;
+        paginas = Math.ceil(locations.length/10);
+        generarPaginacion();
+        let locationspag = locations.slice(10*(actual-1), 10*actual);
+        for(let i=0; i<locationspag.length;i++){
+            appendtable(i+1, locationspag[i]);
+        }
+    })
+
+
+
+
+    function appendtable(i, location){
+        let id = location.url.split("/")[6];
+        $('table').append('<tr>'+
+                            '<td>'+i+'</td>'+
+                            '<td>'+location.name+'</td>'+
+                            '<td><a class="btn btn-primary" title="Detalles" href="./detalleRegion/detalleLocacion.html?region='+id+'">Detalles</a></td>'+
+                            '</tr>');
+                        
+    }
+
+
 
 });
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
 $(document).ready(function () {
+    $.ajax({
+        method: "GET",
+        url:"https://pokeapi.co/api/v2/region"
+    }).done((response)=>{
+        let results = response.results;
+        results.forEach(region =>{
+            appendtable(region);
+        })
+    })
 
-
+    function appendtable(region){
+        let id = region.url.split("/")[6];
+        $('table').append('<tr>'+
+                            '<td>'+id+'</td>'+
+                            '<td>'+region.name+'</td>'+
+                            '<td><a class="btn btn-primary" title="Detalles" href="./detalleRegion/detalleRegion.html?region='+id+'">Detalles</a></td>'+
+                            '</tr>');
+                        
+       }
 });
